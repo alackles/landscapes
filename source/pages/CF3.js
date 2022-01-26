62eb2c675039b81b5cfd4b80bbcfa77cb559cc1c
Acacia Ackles <alackles@msu.edu>
2022-01-26 14:15:04 -0500

    Update data pull for landscapes

--- source/pages/CF3.js
+++ source/pages/CF3.js
@@ -1,8 +1,8 @@
 $.getScript("./landscape.js", function()
     {;
         landscape_file = "../../data/coords_CF3.csv";
-        lod_file = "../../data/reps/SEED_0__F_CF3/lod.csv";
-        edges_file = "../../data/reps/SEED_0__F_CF3/edges.csv";
+        lod_file = "../../data/reps/SEED_00__F_CF3__MUT_0001__T_04/lod.csv";
+        edges_file = "../../data/reps/SEED_00__F_CF3__MUT_0001__T_04/edges.csv";
         load_landscape(landscape_file, lod_file, edges_file);
     }
 
--- source/pages/vincent.js
+++ source/pages/vincent.js
@@ -1,8 +1,8 @@
 $.getScript("./landscape.js", function()
     {;
         landscape_file = "../../data/coords_vincent.csv";
-        lod_file = "../../data/reps/SEED_00__F_Vincent/lod.csv";
-        edges_file = "../../data/reps/SEED_00__F_Vincent/edges.csv";
+        lod_file = "../../data/reps/SEED_00__F_Vincent__MUT_0001__T_04/lod.csv";
+        edges_file = "../../data/reps/SEED_00__F_Vincent__MUT_0001__T_04/edges.csv";
         load_landscape(landscape_file, lod_file, edges_file);
     }
 
